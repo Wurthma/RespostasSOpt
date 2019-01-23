86fd5d7a031c0cee3c26928c4e218a835a48a331
Wurthmann <ge_h_w@hotmail.com>
2019-01-22 22:49:54 -0200

    Exemplo para pergunta 306099.

---
+++ Resposta_306099/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resposta_306099.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileStreamResult DownloadImagem()
+        {
+            //Converter byte[] to Stream
+            Stream streamImg = new MemoryStream(BuscarImagem());
+            return File(streamImg, "image/png");
+        }
+
+        public FileContentResult DownloadImagem2()
+        {
+            return File(BuscarImagem(), "image/png");
+        }
+
+        private byte[] BuscarImagem()
+        {
+            WebClient wc = new WebClient();
+            byte[] bytesImg = wc.DownloadData("https://cdn.sstatic.net/Sites/stackoverflow/company/img/logos/so/so-logo.png");
+            return bytesImg;
+        }
+    }
+}
---
+++ Resposta_306099/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Resposta_306099.MvcApplication" Language="C#" %>
